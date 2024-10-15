9a87d0e44654eb831f24145477cb7a25a86c0c94
dharlabwustl <atulkumar@wustl.edu>
2024-10-15 11:36:58 -0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+RUN mkdir -p /callfromgithub
+RUN chmod 755 /callfromgithub
+COPY downloadcodefromgithub.sh /callfromgithub/
+RUN chmod +x /callfromgithub/downloadcodefromgithub.sh
+# Set environment variables for the virtual environment path
+ENV VIRTUAL_ENV=/opt/antspyvenv
+
+# Create the virtual environment
+RUN python -m venv $VIRTUAL_ENV
+
+# Ensure that the virtual environment is used
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+# Install system dependencies for building Python packages (if needed)
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Upgrade pip
+RUN pip install --upgrade pip
+
+# Install ANTsPy in the virtual environment
+RUN pip install antspyx
+
+# Set the default command to Python shell
+CMD ["python"]
---
+++ script_to_call_main_program.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SESSION_ID=${1}
+XNAT_USER=${2}
+XNAT_PASS=${3}
+TYPE_OF_PROGRAM=${4}
+echo TYPE_OF_PROGRAM::${TYPE_OF_PROGRAM}
+#'https://redcap.wustl.edu/redcap/api/' #
+echo ${REDCAP_API}
+#export REDCAP_API=${6}
+#echo REDCAP_API::${REDCAP_API}
+# The input string
+input=$XNAT_HOST ##"one::two::three::four"
+# Check if '::' is present
+if echo "$input" | grep -q "+"; then
+  # Set the delimiter
+  IFS='+'
+
+  # Read the split words into an array
+  read -ra ADDR <<< "$input"
+  export XNAT_HOST=${ADDR[0]} 
+  SUBTYPE_OF_PROGRAM=${ADDR[1]} 
+else
+export XNAT_HOST=${5} 
+    echo "'+' is not present in the string"
+fi
+
+
+echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM::${SUBTYPE_OF_PROGRAM}::${ADDR[0]}::${ADDR[2]}::${ADDR[3]}
+
+
+if [[ ${TYPE_OF_PROGRAM} == 'ANTSPY_NON_LINEAR_REG' ]]; then
+  echo " I AM AT TYPE_OF_PROGRAM==ANTSPY_NON_LINEAR_REG"
+  /software/deepregbasedregis.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+fi
+
+
+
