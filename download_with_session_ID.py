fa845b4dfd507d811e6b9fd1e604d4377160adfc
dharlabwustl <atulkumar@wustl.edu>
2024-10-15 13:40:44 -0500

    First commit

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1258,9 +1258,9 @@ def get_resourcefiles_metadata_saveascsv(URI,resource_dir,dir_to_receive_the_dat
     url = (URI+'/resources/' + resource_dir +'/files?format=json')
     # print("url::{}".format(url))
     xnatSession = XnatSession(username=XNAT_USER, password=XNAT_PASS, host=XNAT_HOST)
-    xnatSession.renew_httpsession()
+    # xnatSession.renew_httpsession()
     response = xnatSession.httpsess.get(xnatSession.host + url)
-    xnatSession.close_httpsession()
+    # xnatSession.close_httpsession()
     metadata_masks=response.json()['ResultSet']['Result']
     # print("metadata_masks::{}".format(metadata_masks))
     df_scan = pd.read_json(json.dumps(metadata_masks))
