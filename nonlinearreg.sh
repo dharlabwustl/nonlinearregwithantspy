1758372722f68a26646c74f14439c839c414b76e
dharlabwustl <atulkumar@wustl.edu>
2024-10-15 13:28:12 -0500

    First commit

--- nonlinearreg.sh
+++ nonlinearreg.sh
@@ -6,7 +6,7 @@ URI='/data/experiments/'${session_id}
 resource_dir='NIFTI_LOCATION'
 dir_to_receive_the_data="/workinginput/"
 output_csvfile=${session_id}_NIFTI_LOCATION.csv
-call_uploadsinglefile_with_URI_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${session_metadata_filename})
+call_uploadsinglefile_with_URI_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${dir_to_receive_the_data} ${output_csvfile})
 outputfiles_present=$(python /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
 
 ## download the required files:
